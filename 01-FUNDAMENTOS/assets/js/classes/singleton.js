531d186e2e5e56a8f88791b92be4a989bbdedc4d
Armando <armando1805@hotmail.com>
2023-02-01 13:06:39 -0700

    Singleton

--- 01-FUNDAMENTOS/assets/js/classes/singleton.js
+++ 01-FUNDAMENTOS/assets/js/classes/singleton.js
@@ -6,6 +6,20 @@ class Singleton {
     nombre = '';
 
     constructor(nombre = ''){
+
+        if(!!Singleton.instancia){
+            return Singleton.instancia;
+        }
         
+        Singleton.instancia = this;
+        this.nombre = nombre;
+
+
     }
 }
+
+const instancia1 = new Singleton('IronMan');
+const instancia2 = new Singleton('Spiderman');
+
+console.log(`Nombre en la instancia1 es: ${instancia1.nombre}`)
+console.log(`Nombre en la instancia1 es: ${instancia2.nombre}`)
